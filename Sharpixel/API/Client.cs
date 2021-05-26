c5c0468df1043132c465f35cafbc1b76f1709bbb
Maksim <maxus.info.mail@gmail.com>
2021-05-26 16:33:10 +0300

    Initial commit

---
+++ Sharpixel/API/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sharpixel.API
+{
+    /// <summary>
+    /// Main class used for requesting data to API
+    /// </summary>
+    public sealed class Client
+    {
+        /// <summary>
+        /// Authentication key for API
+        /// </summary>
+        public string Key
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Authenticate client
+        /// </summary>
+        /// <param name="key">Your hypixel API key</param>
+        public Client(string key)
+        {
+            Key = key;
+        }
+    }
+}
